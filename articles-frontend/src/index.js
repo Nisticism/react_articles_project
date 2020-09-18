527dd25934dd3de5518743a6e7a57805cad9d864
nisticism <nistacular@gmail.com>
2020-09-17 23:09:26 -0700

    Set up the redux store

--- articles-frontend/src/index.js
+++ articles-frontend/src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import usersReducer from "./reducers/users.js";
+
+const reducer = combineReducers({
+  users: usersReducer,
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
